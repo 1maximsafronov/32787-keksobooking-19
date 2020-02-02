b2a2b294a0b8c5486ef5ccfa0b0b724dafca1ab5
Maxim Safronov <1maximsafronov@gmail.com>
2020-02-02 14:12:38 +0700

    Разбил всё по шункциям и организовал всю генерацию и рендер объектов

--- js/main.js
+++ js/main.js
@@ -1,53 +1,92 @@
 'use strict';
 
-// // Получаем элементы с классом map__pin. Используем селектор.
-// var pins = document.querySelectorAll('.map__pin');
-//
-// // Получаем элемент с идентификатором "housing-price". Используем селектор.
-// var housePriceField = document.querySelector('#housing-price');
-//
-// // Добавляем элементу housePriceField класс hidden
-// housePriceField.classList.add('hidden');
-//
-// // Удаляем класс hidden у элемента housePriceField
-// housePriceField.classList.remove('hidden');
+/*
+  ПЛАН ДЕЙСТВИЙ:
 
+  [+] Написать функцию для создания массива из 8 сгенерированных объектов
+  [+] У блока .map убрать класс .map--faded
+  [+] Создать DOM-элементы наоснове данных из массива объектов, соответствующие меткам на карте, и заполнить данными
+  [+] Отрисовать сгенерированные DOM-элементы в блок .map__pins, Использовать DocumentFragment
 
-var pitTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+  [+] Функция генерации случайных данных
+  [+] Функция создания DOM-элемента на основе объекта
+  [+] Функция заполнения блока DOM-элементами на основе массива объектов
+*/
+
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var mapPins = document.querySelector('.map__pins');
 
 document.querySelector('.map').classList.remove('map--faded');
 
-var pinObj = {
-  'author': {
-    'avatar': 'img/avatars/user0' + (Math.floor(Math.random() * 8) + 1) + '.png'
-  },
-  'offer': {
-    'title': 'заголовок предложения',
-    'address': '{{location.x}}, {{location.y}}',
-    'price': 12345,
-    'type': 'house',
-    'rooms': 2,
-    'guests': 5,
-    'checkin': '12:00',
-    'checkout': '14:00',
-    'features': ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-    'description': '',
-    'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
-  },
-  'location': {
-    'x': 100,
-    'y': 150
+// Функция генерации массива
+var generatePins = function (count) {
+  var pinsArr = [];
+  var offerTypeArr = ['palace', 'flat', 'house', 'bungalo'];
+  for (var i = 0; i < count; i++) {
+    // от ширины контенера .map__pins
+    var locationX = Math.floor(Math.random() * mapPins.offsetWidth);
+    // от 130 до 630
+    var locationY = Math.floor(Math.random() * 500) + 130;
+    var offerType = offerTypeArr[Math.floor(Math.random() * 4)];
+    // Структура объекта
+    var pinObj = {
+      'author': {
+        'avatar': 'img/avatars/user0' + (1 + i) + '.png'
+      },
+      'offer': {
+        'title': 'заголовок предложения', // (пока оставил увсех одинаково)
+        'address': locationX + ', ' + locationY + '',
+        'price': 12345, // стоимость проживания (пока оставил увсех одинаково)
+        'type': offerType,
+        'rooms': 2,   // количество комнат. (пока оставил увсех одинаково)
+        'guests': 5,  // Количество гостей (пока оставил увсех одинаково)
+        'checkin': '12:00', // (пока оставил увсех одинаково)
+        'checkout': '14:00', // (пока оставил увсех одинаково)
+        'features': [  // (пока оставил увсех одинаково)
+          'wifi',
+          'dishwasher',
+          'parking',
+          'washer',
+          'elevator',
+          'conditioner'
+        ],
+        'description': 'Строка с описанием', // (пока оставил увсех одинаково)
+        'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+      },
+      'location': {
+        'x': locationX,
+        'y': locationY
+      }
+    };
+
+    pinsArr[i] = pinObj;
+    pinObj = null;
   }
+  return pinsArr;
 };
 
+// Функция создания DOM-элемента на основе объекта
+var renderPinElement = function (pin) {
+  var pinElement = pinTemplate.cloneNode(true);
 
-var pinElement = pitTemplate.cloneNode(true);
+  pinElement.style = 'left: ' + pin.location.x + 'px; top: ' + pin.location.y + 'px;';
+  pinElement.querySelector('img').src = pin.author.avatar;
+  pinElement.querySelector('img').alt = pin.offer.title;
 
+  return pinElement;
+};
 
-pinElement.style = 'left: ' + pinObj.location.x + 'px; top: ' + pinObj.location.y + 'px;';
-pinElement.querySelector('img').src = pinObj.author.avatar;
-pinElement.querySelector('img').alt = pinObj.offer.title;
+// Функция заполнения блока элементами
+var setPinElements = function (arr) {
+  var fragment = document.createDocumentFragment();
+  for (var j = 0; j < arr.length; j++) {
+    fragment.appendChild(renderPinElement(arr[j]));
+  }
 
+  return fragment;
+};
 
-var mapPins = document.querySelector('.map__pins');
-mapPins.appendChild(pinElement);
+// Вызов генерации массиваобъекта с данными
+var pins = generatePins(8);
+// Отрисовка обектов в контенер на страницу
+mapPins.appendChild(setPinElements(pins));
