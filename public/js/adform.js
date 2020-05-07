fd70dfe9510305dc3df7ede14d559d1b474575bb
1maximsafronov <1maximsafronov@gmail.com>
2020-05-07 20:27:41 +0700

    ...

--- public/js/adform.js
+++ public/js/adform.js
@@ -2,57 +2,40 @@
   const FILE_TYPES = [`gif`, `jpg`, `jpeg`, `png`];
   // Форма ввода данных объявления
   const adForm = document.querySelector(`.ad-form`);
-  const adTitle = adForm.querySelector(`input[id="title"]`);
   const adType = adForm.querySelector(`select[id="type"]`);
+  const adTitle = adForm.querySelector(`input[id="title"]`);
   const adPrice = adForm.querySelector(`input[id="price"]`);
-  const adRoomNumber = adForm.querySelector(`select[id="room_number"]`);
-  const adCapacity = adForm.querySelector(`select[id="capacity"]`);
   const adTimeIn = adForm.querySelector(`select[id="timein"]`);
   const adTimeOut = adForm.querySelector(`select[id="timeout"]`);
   const adAddress = adForm.querySelector(`input[id="address"]`);
-  adAddress.tabIndex = -1;
+  const adCapacity = adForm.querySelector(`select[id="capacity"]`);
+  const adRoomNumber = adForm.querySelector(`select[id="room_number"]`);
   const avatarChooser = adForm.querySelector(`input[id="avatar"]`);
   const avatarPreview = adForm.querySelector(`.ad-form-header__preview img`);
   const imagesChooser = adForm.querySelector(`input[id="images"]`);
-  const adImagesContainer = adForm.querySelector(`.ad-form__photo`);
   const adResetButton = adForm.querySelector(`.ad-form__reset`);
+  const adImagesContainer = adForm.querySelector(`.ad-form__photo`);
+  adAddress.tabIndex = -1;
+
   const successTemplate = document.querySelector(`#success`).content.querySelector(`.success`);
   const errorTemplate = document.querySelector(`#error`).content.querySelector(`.error`);
 
+  // Функция задания значения поля адреса
   const setAddressValue = (x, y) => {
     adAddress.value = x + `, ` + y + ``;
   };
 
+  // Функция выделения невалидных полей
   const setInvalidOutline = (target) => {
     target.style.outline = `2px solid red`;
   };
 
+  // функция оцистки выделения невалидных полей
   const clearInvalidOutline = (target) => {
     target.style.outline = `none`;
   };
 
-  const onAdTitleInvalid = () => {
-    let errorMessage = ``;
-    if (adTitle.validity.tooShort) {
-      errorMessage = `Заголовок должен состоять минимум из 30-ти символов`;
-    } else if (adTitle.validity.tooLong) {
-      errorMessage = `Заголовок не должен привышать 100 символов`;
-    } else if (adTitle.validity.valueMissing) {
-      errorMessage = `Обязательное поле`;
-    }
-    adTitle.setCustomValidity(errorMessage);
-    setInvalidOutline(adTitle);
-  };
-
-  const onAdTitleInput = (evt) =>{
-    let target = evt.target;
-    if (target.value.length < 30) {
-      target.setCustomValidity(`Имя должно состоять минимум из ` + 30 + `-x символов`);
-    } else {
-      target.setCustomValidity(``);
-    }
-  };
-
+  // Функция валидации типа выбраного жилья
   const validateAdType = () => {
     let placeholderPrice = {
       'bungalo': 0,
@@ -62,10 +45,6 @@
     };
     adPrice.placeholder = placeholderPrice[adType.value];
     adPrice.min = placeholderPrice[adType.value];
-  };
-
-  const onAdTypeChange = () => {
-    validateAdType();
   };
 
   const getErrorCapacity = (rooms, capacity) => {
@@ -81,6 +60,7 @@
     return ``;
   };
 
+  // Валидация вместимости жилья
   const validateRoomsCapacity = () => {
     let rooms = parseInt(adRoomNumber.value, 10);
     let capacity = parseInt(adCapacity.value, 10);
@@ -88,58 +68,13 @@
     adCapacity.setCustomValidity(errorMessage);
   };
 
-  const onCapacityChange = () => {
-    validateRoomsCapacity();
-  };
-
-  const onRoomNumberChange = () => {
-    validateRoomsCapacity();
-  };
-
-  const onAvatarChooserChange = () => {
-    let file = avatarChooser.files[0];
-    let fileName = file.name.toLowerCase();
-    let matches = FILE_TYPES.some((it) => {
-      return fileName.endsWith(it);
-    });
-
-    if (matches) {
-      let reader = new FileReader();
-
-      reader.addEventListener(`load`, () => {
-        avatarPreview.src = reader.result;
-      });
-
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const onImagesChooserChange = () => {
-    let file = imagesChooser.files[0];
-    let fileName = file.name.toLowerCase();
-    let matches = FILE_TYPES.some((it) => {
-      return fileName.endsWith(it);
-    });
-    if (matches) {
-      let reader = new FileReader();
-
-      reader.addEventListener(`load`, () => {
-        const img = document.createElement(`img`);
-        img.src = reader.result;
-        img.style.width = 70 + `px`;
-        img.style.height = 70 + `px`;
-        adImagesContainer.appendChild(img);
-      });
-
-      reader.readAsDataURL(file);
-    }
-  };
   // Очистка выбранных фотографий
   const removeImages = () => {
     while (adImagesContainer.firstChild) {
       adImagesContainer.removeChild(adImagesContainer.firstChild);
     }
   };
+
   // При успешной отправке формы
   const onFormSuccess = () => {
     const successMessage = successTemplate.cloneNode(true);
@@ -204,33 +139,114 @@
 
   };
 
-  // Функция при отправке формы объявления
-  const onAdFormSubmit = (evt) => {
-    evt.preventDefault();
-    window.backend.upload(new FormData(adForm), onFormSuccess, onFormError);
-  };
 
-  adTitle.addEventListener(`invalid`, onAdTitleInvalid);
-  adTitle.addEventListener(`input`, onAdTitleInput);
-  adType.addEventListener(`change`, onAdTypeChange);
+  // При вводе текста в поле загаловка
+  adTitle.addEventListener(`input`, (evt) =>{
+    let target = evt.target;
+    if (target.value.length < 30) {
+      target.setCustomValidity(`Имя должно состоять минимум из ` + 30 + `-x символов`);
+    } else {
+      target.setCustomValidity(``);
+    }
+  });
+
+  // При невалидном после ввода загаловка
+  adTitle.addEventListener(`invalid`, () => {
+    let errorMessage = ``;
+    if (adTitle.validity.tooShort) {
+      errorMessage = `Заголовок должен состоять минимум из 30-ти символов`;
+    } else if (adTitle.validity.tooLong) {
+      errorMessage = `Заголовок не должен привышать 100 символов`;
+    } else if (adTitle.validity.valueMissing) {
+      errorMessage = `Обязательное поле`;
+    }
+    adTitle.setCustomValidity(errorMessage);
+    setInvalidOutline(adTitle);
+  });
+
+  // При изменении типа жилья
+  adType.addEventListener(`change`, () => {
+    validateAdType();
+  });
+
+  // при невалидной цене
   adPrice.addEventListener(`invalid`, () => {
     setInvalidOutline(adPrice);
   });
+
+  // При изменении времени въезда в квартиру
   adTimeIn.addEventListener(`change`, () => {
     adTimeOut.value = adTimeIn.value;
   });
+
+  // При изменении времени выезда из квартиры
   adTimeOut.addEventListener(`change`, () => {
     adTimeIn.value = adTimeOut.value;
   });
-  adRoomNumber.addEventListener(`change`, onRoomNumberChange);
-  adCapacity.addEventListener(`change`, onCapacityChange);
+
+  // При изменении количества комнат
+  adRoomNumber.addEventListener(`change`, () => {
+    validateRoomsCapacity();
+  });
+
+  // При изменении элемента вместимости комнат
+  adCapacity.addEventListener(`change`, () => {
+    validateRoomsCapacity();
+  });
+
+  // При невалидной вместимости комнат
   adCapacity.addEventListener(`invalid`, () => {
     setInvalidOutline(adCapacity);
   });
-  avatarChooser.addEventListener(`change`, onAvatarChooserChange);
-  imagesChooser.addEventListener(`change`, onImagesChooserChange);
-  adForm.addEventListener(`submit`, onAdFormSubmit);
 
+  // при изменении элемента загрузки аватара
+  avatarChooser.addEventListener(`change`, () => {
+    let file = avatarChooser.files[0];
+    let fileName = file.name.toLowerCase();
+    let matches = FILE_TYPES.some((it) => {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      let reader = new FileReader();
+
+      reader.addEventListener(`load`, () => {
+        avatarPreview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+  // при изменении элемента выбора изображения
+  imagesChooser.addEventListener(`change`, () => {
+    let file = imagesChooser.files[0];
+    let fileName = file.name.toLowerCase();
+    let matches = FILE_TYPES.some((it) => {
+      return fileName.endsWith(it);
+    });
+    if (matches) {
+      let reader = new FileReader();
+
+      reader.addEventListener(`load`, () => {
+        const img = document.createElement(`img`);
+        img.src = reader.result;
+        img.style.width = 70 + `px`;
+        img.style.height = 70 + `px`;
+        adImagesContainer.appendChild(img);
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+  // Функция при отправке формы объявления
+  adForm.addEventListener(`submit`, (evt) => {
+    evt.preventDefault();
+    window.backend.upload(new FormData(adForm), onFormSuccess, onFormError);
+  });
+
+  // При нажатии на кномку сброса формы
   adResetButton.addEventListener(`click`, (evt) => {
     evt.preventDefault();
     // window.main.deactivatePage();
@@ -238,13 +254,14 @@
     window.map.disable();
   });
 
-
+  // Функция включения формы объявления
   const enable = () => {
     adForm.classList.remove(`ad-form--disabled`);
     window.utils.enableForm(adForm);
     validateRoomsCapacity();
   };
 
+  // Функция отключения формы объявления
   const disable = () => {
     adForm.classList.add(`ad-form--disabled`);
     window.utils.disableForm(adForm);
@@ -263,3 +280,15 @@
     disable
   };
 })();
+
+
+/*
+onAdFormSubmit
+onImagesChooserChange
+onAvatarChooserChange
+onCapacityChange
+onRoomNumberChange
+onAdTypeChange
+onAdTitleInput
+onAdTitleInvalid
+*/
